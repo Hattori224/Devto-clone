2fe43fbd6cb5554b396f2ed29373fe2401cf792d
robinAny <robin0224@mail.ru>
2021-10-16 20:55:33 -0700

    first commit

---
+++ components/navbar.vue
@@ -0,0 +1,9 @@
+<template>
+  <header class="fixed top-0 left-0 w-full bg-white border-b">
+    <div class="container mx-auto flex items-center py-3">
+      <logo />
+      <search />
+      <navbar-actions />
+    </div>
+  </header>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <main class="min-h-screen bg-gray-200" style="margin-top: 65px">
+    <section class="container mx-auto py-8">
+      <div class="grid grid-cols-4 gap-4">
+        <div>
+          <ul class="space-y-4">
+            <li class="h-8 rounded bg-red-400" v-for="n in 7" :key="n"></li>
+          </ul>
+        </div>
+        <div class="col-span-2">
+          <ul class="space-y-4">
+            <li class="h-64 rounded bg-green-400" v-for="n in 20" :key="n"></li>
+          </ul>
+        </div>
+        <div>
+          <ul class="space-y-4">
+            <li class="h-8 rounded bg-blue-400" v-for="n in 7" :key="n"></li>
+          </ul>
+        </div>
+      </div>
+    </section>
+  </main>
+</template>
+
+<script>
+export default {}
+</script>
